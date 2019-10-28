ab120e8a8ab1edfaea5a16cff104c8b303df5952
Valdas Žemaitis <valdas@idega.com>
2019-10-28 14:12:52 +0200

    Added toString

--- src/java/com/idega/restful/bean/LocalizedText.java
+++ src/java/com/idega/restful/bean/LocalizedText.java
@@ -4,22 +4,26 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.idega.util.CoreConstants;
+
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class LocalizedText implements JAXBNatural, Cloneable {
-	
+
 	private String key;
 	private String defaultValue;
-	
+
 	public LocalizedText() {
-		
+		super();
 	}
-	
+
 	public LocalizedText(String key,String defaultValue) {
+		this();
+
 		this.key = key;
 		this.defaultValue = defaultValue;
 	}
-	
+
 	public String getKey() {
 		return key;
 	}
@@ -36,6 +40,11 @@ public class LocalizedText implements JAXBNatural, Cloneable {
 	@Override
 	public LocalizedText clone() throws CloneNotSupportedException {
 		return (LocalizedText)super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return getKey() + CoreConstants.EQ + getDefaultValue();
 	}
 
 }
